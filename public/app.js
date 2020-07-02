b90e24ba7023524fedb64defe905e72d64afa253
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-07-02 11:26:28 -0700

    if user types * now returns all

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
 
 let s;
+let data;
 const dbRefObject = firebase.database().ref().child('animals');
 dbRefObject.on('value', snap => {
+    data = snap.val();
     s = new Solution()
     s.build(snap.val());
 });
@@ -13,6 +15,7 @@ function returnResults() {
     let userText = document.getElementById("animalInput").value.toLowerCase()
     userText = userText.charAt(0).toUpperCase() + userText.slice(1);
     console.log(userText);
+
     if (userText) {
         document.getElementById("suggestions").innerText = "";
         let suggestions = s.autocomplete(userText);
@@ -31,5 +34,21 @@ function returnResults() {
         }
     } else {
         document.getElementById("suggestions").innerText = "";
+    }
+    if (userText == '*') {
+        console.log("giving my all")
+        console.log(data);
+        for (suggestion of data) {
+            let ul = document.getElementById('suggestions');
+            let li = document.createElement('li');
+            let a = document.createElement('a');
+            a.appendChild(document.createTextNode(suggestion));
+            a.setAttribute('href', `https://en.wikipedia.org/wiki/${suggestion}`);
+            a.setAttribute('class', 'subtitle')
+
+            li.appendChild(a);
+
+            ul.appendChild(li)
+        }
     }
 }
