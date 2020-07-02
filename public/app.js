b13d5710c760488ae3ea0e8c3c32df14b264b916
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-07-02 10:43:37 -0700

    now looks for animal in database

---
+++ public/app.js
@@ -0,0 +1,35 @@
+
+let s;
+const dbRefObject = firebase.database().ref().child('animals');
+dbRefObject.on('value', snap => {
+    s = new Solution()
+    s.build(snap.val());
+});
+
+
+
+function returnResults() {
+    console.log("something happened");
+    let userText = document.getElementById("animalInput").value.toLowerCase()
+    userText = userText.charAt(0).toUpperCase() + userText.slice(1);
+    console.log(userText);
+    if (userText) {
+        document.getElementById("suggestions").innerText = "";
+        let suggestions = s.autocomplete(userText);
+        suggestions.pop();
+        for (suggestion of suggestions) {
+            let ul = document.getElementById('suggestions');
+            let li = document.createElement('li');
+            let a = document.createElement('a');
+            a.appendChild(document.createTextNode(suggestion));
+            a.setAttribute('href', `https://en.wikipedia.org/wiki/${suggestion}`);
+            a.setAttribute('class', 'subtitle')
+
+            li.appendChild(a);
+
+            ul.appendChild(li)
+        }
+    } else {
+        document.getElementById("suggestions").innerText = "";
+    }
+}
